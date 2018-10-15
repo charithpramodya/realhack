6a901fccacdb58cc931e70e8f4950368717c9358
charithpramodya <pramodyacharithc@gmail.com>
2018-10-16 04:26:28 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -8,12 +8,12 @@ class DB{
 	var $password = "";
 	var $db="ellipsischat";
 	var $conn;
-	
+	$this->conn = new mysqli(null, "root", "", $db, 0, ":/cloudsql/realhackfi:realhackfinal");
+	if ($this->conn->connect_error) {
+	    	die("Connection failed: " . $this->conn->connect_error);
+	} 
 	function __construct(){
-		$this->conn = new mysqli(null, "root", "", $db, 0, ":/cloudsql/realhackfi:realhackfinal");
-		if ($this->conn->connect_error) {
-	    		die("Connection failed: " . $this->conn->connect_error);
-		} 
+		
 		
 	}
 
